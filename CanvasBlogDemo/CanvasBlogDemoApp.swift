a3189c8f1852f5524576ec0baa5a0dbd11dc2169
Mark Townsend <mark@markltownsend.com>
2024-09-18 05:37:11 -0700

    Initial Commit

---
+++ CanvasBlogDemo/CanvasBlogDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasBlogDemoApp.swift
+//  CanvasBlogDemo
+//
+//  Created by Mark Townsend on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct CanvasBlogDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CanvasBlogDemoTests/CanvasBlogDemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  CanvasBlogDemoTests.swift
+//  CanvasBlogDemoTests
+//
+//  Created by Mark Townsend on 9/18/24.
+//
+
+import Testing
+
+struct CanvasBlogDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
